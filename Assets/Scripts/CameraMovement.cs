4bfe3a7181b94711f8948d4e220d97c74315d0ae
Neviroh <Vironv@yandex.ru>
2017-10-08 14:46:08 +0400

    Cleaned up code and added some unnecessary particles

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
+    [Header("Tag for world space")]
     public string earthTag = "Plane";
 
+    [Header("Moving parameters")]
+    public float movingSpeed = .5f;
     public float afterMovingSlowdown = .5f;
     public float afterMovingDivisionSpeed = 2f;
 
-    public float movingSpeed = .5f;
+    [Header("Zoomimg parameters")]
+    public float minOrthoSize = 50f;
+    public float maxOrthoSize = 150f;
+    public float orthoZoomSpeed = .5f;
     public float thresholdBeforeZoom = .5f;
 
     [Header("Rotate world stuff")]
@@ -22,15 +28,13 @@ public class CameraMovement : MonoBehaviour {
     public float maxAngle = 80;
     public float maxRelativeSize = 4f;
 
-    [Header("Orthografic camera parameters")]
-    public float minOrthoSize = 50f;
-    public float maxOrthoSize = 150f; 
-    public float orthoZoomSpeed = .5f;
+    private Camera cameraComponent;
 
-    private Camera camera;
+    private Touch[] touches;
+
     private float oldAngle;
     private Vector3 lookPoint;
-    private bool swipeBegan;
+    private bool rotationBegining;
 
     private bool isAfterMoving;
     private Vector3 vectorAfterMoving;
@@ -38,10 +42,12 @@ public class CameraMovement : MonoBehaviour {
 
     private void Start()
     {
+        touches = new Touch[2];
+
         isAfterMoving = false;
-        swipeBegan = false;
-        camera = GetComponent<Camera>();
-        if (camera == null)
+        rotationBegining = false;
+        cameraComponent = GetComponent<Camera>();
+        if (cameraComponent == null)
         {
             Debug.Log("There is no camera object for cameras moves script!");
         }
@@ -50,64 +56,24 @@ public class CameraMovement : MonoBehaviour {
     }
 
     void Update () {
-        AfterMoving();
-        CheckTouchInput();
-	}
-
-    private void AfterMoving()
-    {
         if (isAfterMoving)
         {
-            afterMovingSpeed -= afterMovingSlowdown * Time.deltaTime;
-            if (afterMovingSpeed <= 0)
-            {
-                isAfterMoving = false;
-            }
-            else
-            {
-                transform.Translate(vectorAfterMoving * afterMovingSpeed);
-
-                if (SetLookPoint() == false)
-                {
-                    transform.Translate(-vectorAfterMoving * afterMovingSpeed);
-                    isAfterMoving = false;
-                }
-            }
+            AfterMoving();
         }
-    }
 
-    private void CheckTouchInput()
-    {
         if (Input.touchCount == 1)
         {
-            Touch touch = Input.GetTouch(0);
-            transform.Translate(-touch.deltaPosition * movingSpeed);
-
-            if (SetLookPoint() == false)
-            {
-                transform.Translate(touch.deltaPosition * movingSpeed);
-            }
-            else if (touch.phase == TouchPhase.Ended)
-            {
-                vectorAfterMoving = -touch.deltaPosition ;
-                afterMovingSpeed = movingSpeed / afterMovingDivisionSpeed;
-                isAfterMoving = true;
-            }
+            MovingCamera();
         }
         else if (Input.touchCount == 2)
         {
-            Touch[] touches = new Touch[2];
-            for (int i = 0; i < 2; i++)
-            {
-                touches[i] = Input.GetTouch(i);
-            }
-
-            CheckZoom(touches);
-            CheckWorldRotate(touches);
-            CheckUpAngleDiff(touches);
+            DoubleTouchCheck();
         }
-    }
+	}
 
+    //
+    // BLOCK: Look point
+    //
     bool SetLookPoint()
     {
         bool foundLookPoint = false;
@@ -126,7 +92,22 @@ public class CameraMovement : MonoBehaviour {
         return foundLookPoint;
     }
 
-    private void CheckZoom(Touch[] touches)
+    //
+    // BLOCK: camera moves (zoom, rotation)
+    //
+    private void DoubleTouchCheck()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            touches[i] = Input.GetTouch(i);
+        }
+
+        CheckZoom();
+        CheckWorldRotate();
+        CheckUpAngleDiff();
+    }
+
+    private void CheckZoom()
     {
         Vector2[] prevPositions = new Vector2[2];
         for (int i = 0; i < 2; i++)
@@ -139,21 +120,16 @@ public class CameraMovement : MonoBehaviour {
         float deltaDistance = currentDistance - prevDistance;
         if (Mathf.Abs(deltaDistance) > thresholdBeforeZoom)
         {
-            if (camera.orthographic)
+            if (cameraComponent.orthographic)
             {
-                camera.orthographicSize -= deltaDistance * orthoZoomSpeed;
-                camera.orthographicSize = Mathf.Clamp(camera.orthographicSize, minOrthoSize, maxOrthoSize); 
+                cameraComponent.orthographicSize -= deltaDistance * orthoZoomSpeed;
+                cameraComponent.orthographicSize = Mathf.Clamp(cameraComponent.orthographicSize, minOrthoSize, maxOrthoSize); 
             }
         }
     }
 
-    private void CheckUpAngleDiff(Touch[] touches)
+    private void CheckUpAngleDiff()
     {
-        if (touches[0].phase != TouchPhase.Moved || touches[1].phase != TouchPhase.Moved)
-        {
-            return;
-        }
-
         float maxVectorsMagnitude = Mathf.Max(touches[0].deltaPosition.magnitude,touches[1].deltaPosition.magnitude);
         float sumMagnitude = (touches[0].deltaPosition + touches[1].deltaPosition).magnitude;
         float relativeSize = touches[0].deltaPosition.magnitude / touches[1].deltaPosition.magnitude;
@@ -163,25 +139,24 @@ public class CameraMovement : MonoBehaviour {
             float averageRotate = (touches[0].deltaPosition.y + touches[1].deltaPosition.y) / 2;
 
             transform.RotateAround(lookPoint, transform.right, -averageRotate * upAngleSpeed);
-            if (transform.rotation.eulerAngles.x > maxAngle
-                || transform.rotation.eulerAngles.x < minAngle)
+            if (transform.rotation.eulerAngles.x > maxAngle || transform.rotation.eulerAngles.x < minAngle)
             {
                 transform.RotateAround(lookPoint, transform.right, averageRotate * upAngleSpeed);
             }
         }
     }
 
-    private void CheckWorldRotate(Touch[] touches)
+    private void CheckWorldRotate()
     {
         for (int i = 0; i < 2; i++)
         {
             if (touches[i].phase == TouchPhase.Began)
             {
-                swipeBegan = true;
+                rotationBegining = true;
             }
         }
 
-        if (!swipeBegan)
+        if (!rotationBegining)
         {
             Vector3 angleVector = touches[1].position - touches[0].position;
             float newAngle = Mathf.Atan2(angleVector.x, angleVector.y) *Mathf.Rad2Deg;
@@ -194,7 +169,51 @@ public class CameraMovement : MonoBehaviour {
         {
             Vector3 oldAngleVector = touches[1].position - touches[0].position;
             oldAngle = Mathf.Atan2(oldAngleVector.x, oldAngleVector.y) * Mathf.Rad2Deg;
-            swipeBegan = false;
+            rotationBegining = false;
         }   
+    }
+
+    //
+    // BLOCK: Moving
+    //
+
+    private void MovingCamera()
+    {
+        touches[0] = Input.GetTouch(0);
+        transform.Translate(-touches[0].deltaPosition * movingSpeed);
+
+        if (SetLookPoint() == false)
+        {
+            transform.Translate(touches[0].deltaPosition * movingSpeed);
+        }
+        else if (touches[0].phase == TouchPhase.Ended)
+        {
+            vectorAfterMoving = -touches[0].deltaPosition;
+            afterMovingSpeed = movingSpeed / afterMovingDivisionSpeed;
+            isAfterMoving = true;
+        }
+    }
+
+    /*
+     * Moves camera after end of touch input
+     * The faster user swipes, the farther camera moves after
+     */
+    private void AfterMoving()
+    {
+        afterMovingSpeed -= afterMovingSlowdown * Time.deltaTime;
+        if (afterMovingSpeed <= 0)
+        {
+            isAfterMoving = false;
+        }
+        else
+        {
+            transform.Translate(vectorAfterMoving * afterMovingSpeed);
+
+            if (SetLookPoint() == false)
+            {
+                transform.Translate(-vectorAfterMoving * afterMovingSpeed);
+                isAfterMoving = false;
+            }
+        }
     }
 }
